9d61978435ae61b10c44c34263dc37a10393353c
Tejash <godavarthitejash@gmail.com>
2025-02-22 18:30:08 +0530

    first commit

---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+import NewsItem from "./NewsItem"
+function NewsBoard({ category }) {
+  const [articles, setarticles] = useState([]);
+
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+    fetch(url).then(response => response.json()).then(data => setarticles(data.articles));
+  }, [category]);
+  return (
+    <div>
+      <h2 className="text-center" style={{ fontSize: "50px" }}>Latest <span className="badge bg-danger">News</span></h2>
+      {articles.map((news, index) => {
+        return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />;
+      })}
+    </div>
+  );
+}
+
+export default NewsBoard
